5af729f651e168340663cc1e7e0af36d588d8324
JosFa1 <123281534+JosFa1@users.noreply.github.com>
2026-03-23 09:18:08 -0400

    Fix weekly schedule pattern for US and MS

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -116,8 +116,8 @@ function Home() {
 
   // Weekly pattern: A, A, B, C, A
   const thisWeek: WeeklySchedule = scheduleType === 'US'
-    ? [ADayUS, BDayUS, CDayUS, ADayUS, NSDay]
-    : [ADayMS, BDayMS, CDayMS, ADayMS, NSDay];
+    ? [ADayUS, ADayUS, BDayUS, CDayUS, ADayUS]
+    : [ADayMS, ADayMS, BDayMS, CDayMS, ADayMS];
 
   // Get today's schedule for the global clock
   const todaysSchedule = useMemo(() => {
